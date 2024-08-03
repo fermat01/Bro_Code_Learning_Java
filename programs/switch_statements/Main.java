e04cace73d583d3b249db24a2801be514454e06a
fermat01 <fermat01fils@gmail.com>
2024-08-03 10:06:31 +0200

    add files in programs folder

---
+++ programs/switch_statements/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Switch = statement that allows a variable to be tested for quality against a list of values.
+		String day = "pizza";
+		switch(day) {
+		case "Sunday": System.out.println("It is Sunday !");
+		break;
+		case "Monday": System.out.println("It is Monday !");
+		break;
+		case "Tuesaday": System.out.println("It is Tuesady !");
+		break;
+		case "Wednesady": System.out.println("It is Wednesday !");
+		break;
+		case "Thursday": System.out.println("It is Thursday !");
+		break;
+		case "Friday": System.out.println("It is Friday !");
+		break;
+		case "Saturday": System.out.println("It is Saturday");
+		break;
+		default: System.out.println("It is not a day !");
+		
+		}
+
+	}
+
+}
