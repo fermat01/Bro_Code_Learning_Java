555c82bf61e4415dda26a15b8564c288b5fbf801
fermat01 <fermat01fils@gmail.com>
2024-08-07 10:08:32 +0200

    add java files in program folder

---
+++ programs/Arrays/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//  array = used to store multiple value in a single variable
+		String[] cars = new String[5];
+		
+		cars[0] = "Mustang";
+		cars[1] = "Camaro";
+		cars[2] = "Corvette";
+		cars[3] = "Tesla";
+		cars[4] = "Ferrari";
+		for(int i=0; i<cars.length; i++) {
+		System.out.println(cars[i]);
+
+}
+
+}
+}
